7b5f0adc4e43b647d4ac70032592e97917b78009
Varun Sridharan <varunsridharan23@gmail.com>
2022-09-07 19:16:43 +0530

    Updated

--- .github/script/generate.php
+++ .github/script/generate.php
@@ -1,28 +1,109 @@
 <?php
+define('BASE', __DIR__ . '/');
+define('WEBSITE', __DIR__ . '/../../docs/');
+define('FONTS', __DIR__ . '/../../docs/static/fonts/');
 
-$fonts = glob(__DIR__ . '/../../docs/static/fonts/*.*');
+$html = array();
+$css = array();
+$category = array('all' => 'All');
+$fonts = array();
+function base64DataUri($sFile)
+{
+    // Switch to right MIME-type
+    $sExt = strtolower(substr(strrchr($sFile, '.'), 1));
 
-$html = '';
-$css = '';
+    switch ($sExt) {
+        case 'gif':
+        case 'jpg':
+        case 'png':
+            $sMimeType = 'image/' . $sExt;
+            break;
 
-if (!empty($fonts)) {
-    foreach ($fonts as $font) {
-        $font = basename($font);
-        $hash = md5($font);
-        $name = explode('.', $font);
-        $name = (isset($name[0])) ? $name[0] : $font;
-        $content = file_get_contents(__DIR__ . '/display.template');
-        $content = str_replace(array('{{FONT_NAME}}', '{{hash}}'), array($name, $hash), $content);
+        case 'ico':
+            $sMimeType = 'image/x-icon';
+            break;
 
-        $content_css = file_get_contents(__DIR__ . '/display-css.template');
-        $content_css = str_replace(array('{{FONT_URL}}', '{{hash}}'), array('../fonts/'.$font, $hash), $content_css);
+        case 'eot':
+            $sMimeType = 'application/vnd.ms-fontobject';
+            break;
 
-        $html .= $content;
-        $css .= $content_css;
+        case 'otf':
+        case 'ttf':
+        case 'woff':
+            $sMimeType = 'application/octet-stream';
+            break;
+
+        default:
+            exit('Invalid extension file!');
+    }
+
+    $sBase64 = base64_encode(file_get_contents($sFile));
+    return "data:$sMimeType;base64,$sBase64";
+}
+
+function loop_dir($dir)
+{
+    global $html, $css, $category, $fonts;
+    $dir = glob($dir . '/*');
+
+    if (is_array($dir) && array_filter($dir) && !empty($dir)) {
+        foreach ($dir as $font) {
+            if (is_dir($font)) {
+                loop_dir($font);
+            } else {
+                $hash = md5($font);
+                $font_file = basename($font);
+                $font_type = strtolower(substr(strrchr($font_file, '.'), 1));
+                if (!in_array($font_type, array('ttf', 'otf'))) {
+                    continue;
+                }
+                $font_path = trim(trim(str_replace(realpath(WEBSITE . '\static\fonts/'), '', realpath($font)), '\\'), '/');
+                $cat = dirname($font_path);
+
+                if ($cat === '.') {
+                    $cat = 'UnCategorized';
+                }
+
+                $category[strtolower($cat)] = $cat;
+                $font_path = str_replace('\\', '/', $font_path);
+                $name = explode('.', $font_file);
+                $name = (isset($name[0])) ? $name[0] : $font_file;
+                #$content = file_get_contents(__DIR__ . '/display.template');
+
+                $content_css = file_get_contents(__DIR__ . '/display-css.template');
+                $content_css = str_replace(array('{{FONT_URL}}', '{{hash}}'), array('"../fonts/' . $font_path . '"', $hash), $content_css);
+                $html[] = array(
+                    'id' => $hash,
+                    'name' => $name,
+                    'category' => strtolower($cat),
+                    'download_file' => $font_path,
+                );#$content;
+                $css[$hash] = $content_css;
+                $fonts[$hash] = $font_path;
+            }
+        }
     }
 }
 
+loop_dir(FONTS);
+$cat_html = array();
+$fonts = json_encode($fonts);
+
+foreach ($category as $key => $cat) {
+    $cat_html[] = '<div class="item item-menu-font-category" data-value="' . $key . '">' . $cat . '</div>';
+}
+
+$fonts_displays = json_encode($html);
+$html = str_replace(array("\n", "\r"), '', file_get_contents(__DIR__ . '/display.template'));
+
+$display_fonts = '<script>
+var fonts =' . $fonts . '; 
+var fonts_displays = ' . $fonts_displays . ';
+var display_template = \'' . $html . '\';
+</script>';
+
 $html_final = file_get_contents(__DIR__ . '/index.html.template');
-$html_final = str_replace('{{HTML}}', $html, $html_final);
-file_put_contents(__DIR__ . '/../../docs/index.html', $html_final);
-file_put_contents(__DIR__ . '/../../docs/static/css/fonts.css', $css);
+$html_final = str_replace('{{HTML}}', $display_fonts, $html_final);
+$html_final = str_replace('{{CATEGORY_HTML}}', implode(PHP_EOL, $cat_html), $html_final);
+file_put_contents(WEBSITE . 'index.html', $html_final);
+file_put_contents(WEBSITE . 'static/css/fonts.css', implode('', $css));
--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -1,17 +1,75 @@
+function render_fonts(start, limit) {
+    var run = 0;
+    //$('#fontslist > .column').remove();
+    for (var i = start; i < fonts_displays.length; i++) {
+        if (run == limit) {
+            break;
+        }
+        var template = display_template;
+        var k = fonts_displays[i];
+        template = template.replaceAll('{{FONT_NAME}}', k['name']);
+        template = template.replaceAll('{{CATEGORY}}', ' category-' + k['name'] + ' category-all');
+        template = template.replaceAll('{{EXAMPLE}}', $("#input").val());
+        template = template.replaceAll('{{PREVIEW_CSS_CLASS}}', 'font-' + k['id']);
+        template = template.replaceAll('{{hash}}', k['id']);
+        $("#fontslist").append(template);
+        run++;
+    }
+    var showing = $("#fontslist > div").length;
+    var total = fonts_displays.length;
+    $("#showings").html(showing + ' of ' + total + ' Displayed');
+}
+
 $(document).ready(() => {
-  $(".ui.dropdown").dropdown();
+    $(".ui.dropdown").dropdown();
 
-  // Change input content
-  $("#input").on("input", function () {
-    const newText = $(this).val();
+    // Change input content
+    $("#input").on("input", function () {
+        const newText = $(this).val();
 
-    $(".preview-text").text(newText);
-  });
+        $(".preview-text").text(newText);
+    });
 
-  // Change font size
-  $("#preview-font-size").change(function () {
-    const newFontSizeValue = $(this).val();
+    // Change font size
+    $("#preview-font-size").change(function () {
+        const newFontSizeValue = $(this).val();
 
-    $(".preview-text").css("font-size", `${newFontSizeValue}px`);
-  });
+        $(".preview-text").css("font-size", `${newFontSizeValue}px`);
+    });
+
+    $("body").on("click", '.preview-text-container', function () {
+        $(this).find('> .ui').toggleClass('font-selected');
+    });
+
+    $("body").on("click", '.preview-text-container i.bold', function () {
+        $('p#' + $(this).attr('id')).toggleClass('font-bold');
+        $(this).toggleClass('active');
+    });
+
+
+    $("body").on("click", '.preview-text-container i.italic', function () {
+        $('p#' + $(this).attr('id')).toggleClass('font-italic');
+        $(this).toggleClass('active');
+    });
+
+
+    // Change font size
+    $(".item-menu-font-category").click(function () {
+        var cat = $(this).data("value");
+
+        $('.category-all').hide('slow');
+        $('.category-' + cat).show('fast');
+    });
+
+    var start = 0;
+    var limit = 50;
+    render_fonts(0, limit);
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            start = (start + limit);
+            render_fonts(start, limit);
+            $(window).scrollTop($(window).scrollTop() - 1);
+        }
+    });
 });
